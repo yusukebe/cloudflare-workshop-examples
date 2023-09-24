246d3c7483554bab5c0e2597f54ba48521b3d2a3
Yusuke Wada <yusuke@kamawada.com>
2023-09-24 15:51:51 +0900

    update

--- projects/starter-web-api/src/index.ts
+++ projects/starter-web-api/src/index.ts
@@ -1,12 +1,12 @@
+import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
 import { z } from 'zod'
-import { zValidator } from '@hono/zod-validator'
 
 import { renderer } from './renderer'
 
 const schema = z.object({
   title: z.string().min(1),
-  content: z.string().min(1)
+  content: z.string().min(1),
 })
 
 export type Bindings = {
@@ -22,19 +22,28 @@ const app = new Hono<{
   Bindings: Bindings
 }>()
 
+app.get('*', async (c, next) => {
+  c.setRenderer(renderer(c))
+  await next()
+})
+
 app.get('/posts', async (c) => {
-  const { results } = await c.env.DB.prepare('SELECT * FROM posts ORDER BY created_at DESC;').all<Post>()
+  const { results } = await c.env.DB.prepare(
+    'SELECT * FROM posts ORDER BY created_at DESC;'
+  ).all<Post>()
   const posts = results
 
   return c.json({
-    posts
+    posts,
   })
 })
 
 app.post('/posts', zValidator('form', schema), async (c) => {
   const { title, content } = c.req.valid('form')
   const id = crypto.randomUUID()
-  const { success } = await c.env.DB.prepare('INSERT INTO posts(id, title, content) values (?, ?, ?)')
+  const { success } = await c.env.DB.prepare(
+    'INSERT INTO posts(id, title, content) values (?, ?, ?)'
+  )
     .bind(id, title, content)
     .run()
   return c.redirect('/posts')
