f95bd8c7202916105584735ab3062f74a8fad8a7
Yusuke Wada <yusuke@kamawada.com>
2025-07-08 11:27:14 +0900

    update 05-web-api and others

--- projects/05-web-api/src/index.ts
+++ projects/05-web-api/src/index.ts
@@ -1,9 +1,37 @@
 import { Hono } from 'hono'
+import * as z from 'zod/v4'
+import { zValidator } from '@hono/zod-validator'
 
-const app = new Hono()
+export const schema = z.object({
+  title: z.string().min(1),
+  content: z.string().min(1),
+})
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+type Post = z.infer<typeof schema> & {
+  created_at: string
+  id: string
+}
+
+const app = new Hono<{ Bindings: CloudflareBindings }>()
+
+app.get('/posts', async (c) => {
+  const { results: posts } = await c.env.DB.prepare(
+    'SELECT * FROM posts ORDER BY created_at DESC;'
+  ).all<Post>()
+  return c.json({
+    posts,
+  })
+})
+
+app.post('/posts', zValidator('form', schema), async (c) => {
+  const { title, content } = c.req.valid('form')
+  const id = crypto.randomUUID()
+  const { success } = await c.env.DB.prepare(
+    'INSERT INTO posts(id, title, content) values (?, ?, ?)'
+  )
+    .bind(id, title, content)
+    .run()
+  return c.json({ success })
 })
 
 export default app
